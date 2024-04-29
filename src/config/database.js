681b72a1e57cc9b9520a73e76fedef605330317f
Bruce Rodriguez <bruce.rodriguez94@gmail.com>
2024-04-29 16:55:54 -0500

    Se agrego la libreria dotenv para el manejo de variables de entorno

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config({path: __dirname + '../../../.env'})
 
-const sequelize = new Sequelize('rimac_beta', 'root', '', {
-  host: '127.0.0.1',
-  dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_CONNECTION,
 });
 
 module.exports = sequelize;
